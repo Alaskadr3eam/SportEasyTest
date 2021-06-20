c0f8b9ad9118d5fd098a378f88a9c76a2657c9e6
Clément MARTIN <clement.martin1988@gmail.com>
2021-06-20 14:45:24 +0200

    Fix
    - fix: Init commit

---
+++ SportEasyTest/SportEasyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportEasyTest
+//
+//  Created by Clément Martin on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
